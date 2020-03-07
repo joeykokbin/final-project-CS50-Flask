4ff52d3bfe938edb92e34b6aa6d53f41b4950fe3
joeykokbin <50728676+joeykokbin@users.noreply.github.com>
2020-03-07 15:00:52 +0800

    final checks

--- run.py
+++ run.py
@@ -26,17 +26,17 @@ def create_app(test_config = None):
 
         except Exception:
             print("trying this")
-                try:
-                    from main.database import db, Order, Product, User #DB and tables
-                    from main.auth import auth_bp #Authentication blueprint
-                    from main.transact import transact_bp #Website blueprint
-                    from main import auth, transact, routes #import routes
-                except Exception:
-                    print("main.database failed. Trying database")
-                    from database import db, Order, Product, User #DB and tables
-                    from auth import auth_bp #Authentication blueprint
-                    from transact import transact_bp #Website blueprint
-                    import auth, transact, routes #import routes
+            try:
+                from main.database import db, Order, Product, User #DB and tables
+                from main.auth import auth_bp #Authentication blueprint
+                from main.transact import transact_bp #Website blueprint
+                from main import auth, transact, routes #import routes
+            except Exception:
+                print("main.database failed. Trying database")
+                from database import db, Order, Product, User #DB and tables
+                from auth import auth_bp #Authentication blueprint
+                from transact import transact_bp #Website blueprint
+                import auth, transact, routes #import routes
         # Initialise Plugins
         db.init_app(app)
         db.create_all()
