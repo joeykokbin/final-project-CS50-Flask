b7b42e1ccbb07a47b60bd64cd5947cbd194c8647
joeykokbin <50728676+joeykokbin@users.noreply.github.com>
2020-03-10 21:49:13 +0800

    removed comma

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, session, request
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from flask import current_app as app
 from flask import session, Markup, flash
-from database import db, User, Product, Order,
+from database import db, User, Product, Order
 from helpers import apology, allowed_file, modal
 import os
 
